73fac9166c67c0cffa3fd99781031c164cc0c45a
Andrey Ponteleev <ionrussia@gmail.com>
2026-09-22 10:43:31 +0300

    protect accrual repo from updating orders in final status

--- internal/repository/postgres/order_accrual.go
+++ internal/repository/postgres/order_accrual.go
@@ -40,14 +40,15 @@ func (r *OrderAccrualRepository) UpdateOrderAndBalance(ctx context.Context, orde
 		}
 	}
 
-	orderQuery := `UPDATE orders SET status = $1, accrual = $2 WHERE id = $3 AND user_id = $4`
+	//обновить пользователь может только свой заказ в нефинальном статусе
+	orderQuery := `UPDATE orders SET status = $1, accrual = $2 WHERE id = $3 AND user_id = $4 AND status NOT IN ('PROCESSED', 'INVALID')`
 	res, err := tx.Exec(ctx, orderQuery, status, accrual, orderID, userID)
 	if err != nil {
 		return fmt.Errorf("postgres: tx update order failed: %w", err)
 	}
 
 	if res.RowsAffected() == 0 {
-		return fmt.Errorf("postgres: order %s for user %s not found: %w", orderID, userID, repository.ErrOrderNotFound)
+		return fmt.Errorf("postgres: order %s for user %s already processed or not found: %w", orderID, userID, repository.ErrOrderNotFound)
 	}
 
 	if err = tx.Commit(ctx); err != nil {
